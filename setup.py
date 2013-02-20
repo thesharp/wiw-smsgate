e61864c32e3251e6d3b1dbc29a3fa141a30a92b9
Ilya A. Otyutskiy <sharp@thesharp.ru>
2013-02-20 14:47:26 +0400

    Initial import

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="wiw-smsgate",
+    version="0.1",
+    packages=["wiw-smsgate"],
+    # install_requires=[""],
+    author="Ilya A. Otyutskiy",
+    author_email="sharp@thesharp.ru",
+    maintainer="Ilya A. Otyutskiy",
+    url="https://github.com/thesharp/wiw-smsgate",
+    description="wiw-smsgate",
+    license="MIT"
+)
+
